eff30a4e288919cf7846eab577e92701f511d4fc
Forest Gregg <fgregg@uchicago.edu>
2017-04-23 20:20:13 -0500

    manylinux builds

---
+++ .manylinux-install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e -x
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    if [[ "${PYBIN}" == *"cp27"* ]] || \
+       [[ "${PYBIN}" == *"cp34"* ]] || \
+       [[ "${PYBIN}" == *"cp35"* ]] || \
+       [[ "${PYBIN}" == *"cp36"* ]]; then
+        "${PYBIN}/pip" install numpy scipy
+        "${PYBIN}/pip" install -e /io/
+	(cd /io && "${PYBIN}/python" setup.py test)
+        "${PYBIN}/pip" wheel /io/ -w dist/
+        rm -rf /io/build /io/*.egg-info
+    fi
+done
+
+# Bundle external shared libraries into the wheels
+for whl in dist/fastcluster*.whl; do
+    auditwheel repair "$whl" -w /io/dist/
+done
