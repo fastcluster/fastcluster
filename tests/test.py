8baf1cb51e155a0032c12f0dbdcbcafc11fef386
Daniel Müllner <daniel@danifold.net>
2019-12-30 22:39:12 +0100

    Update to version 1.1.26.

--- tests/test.py
+++ tests/test.py
@@ -11,7 +11,7 @@ import numpy as np
 from scipy.spatial.distance import pdist, squareform
 import math
 
-version = '1.1.25'
+version = '1.1.26'
 if fc.__version__ != version:
     raise ValueError('Wrong module version: {} instead of {}.'.format(fc.__version__, version))
 
