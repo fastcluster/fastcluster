35a9a2b0bc9bc4cc835df6a34a792fd44611be89
odidev <odidev@puresoftware.com>
2021-03-16 10:25:34 +0000

    Add linux aacrh64 wheel support

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -2,13 +2,16 @@
 
 set -e -x
 
+arch=$(uname -m)
+
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    if [[ "${PYBIN}" == *"cp27"* ]] || \
-       [[ "${PYBIN}" == *"cp35"* ]] || \
-       [[ "${PYBIN}" == *"cp36"* ]] || \
+    if [[ "${PYBIN}" == *"cp27"* && $arch != "aarch64" ]] || \
+       [[ "${PYBIN}" == *"cp35"* && $arch != "aarch64" ]] || \
+       [[ "${PYBIN}" == *"cp36"* && $arch != "aarch64" ]] || \
        [[ "${PYBIN}" == *"cp37"* ]] || \
-       [[ "${PYBIN}" == *"cp38"* ]]; then
+       [[ "${PYBIN}" == *"cp38"* ]] || \
+       [[ "${PYBIN}" == *"cp39"* ]]; then
         "${PYBIN}/pip" install numpy scipy
         "${PYBIN}/pip" install -e /io/
 	(cd /io && "${PYBIN}/python" setup.py test)
