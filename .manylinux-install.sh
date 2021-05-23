574a2e98f673fb53fa2a2acfab9de73e579ba964
Daniel Müllner <daniel@danifold.net>
2021-05-23 19:46:09 +0200

    Update.

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -2,12 +2,13 @@
 
 set -e -x
 
+cd /io
+
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     if [[ "${PYBIN}" == *"cp37"* ]] || \
        [[ "${PYBIN}" == *"cp38"* ]] || \
        [[ "${PYBIN}" == *"cp39"* ]]; then
-        cd /io
         "${PYBIN}/pip" install --upgrade pip wheel
         "${PYBIN}/pip" install .[test]
         "${PYBIN}/python" setup.py bdist_wheel
