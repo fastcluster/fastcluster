891d6a54bc8db22bf1806e562ea494c3662eb954
Daniel Müllner <daniel@danifold.net>
2021-05-23 19:17:22 +0200

    Update.

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -9,14 +9,25 @@ for PYBIN in /opt/python/*/bin; do
     if [[ "${PYBIN}" == *"cp35"* && $arch != "aarch64" ]] || \
        [[ "${PYBIN}" == *"cp36"* && $arch != "aarch64" ]] || \
        [[ "${PYBIN}" == *"cp37"* ]] || \
-       [[ "${PYBIN}" == *"cp38"* ]]; then
-        "${PYBIN}/pip" install numpy scipy
-        "${PYBIN}/pip" install -e /io/
-	(cd /io && "${PYBIN}/python" setup.py test)
-        "${PYBIN}/pip" wheel /io/ -w dist/
-        rm -rf /io/build /io/*.egg-info
+       [[ "${PYBIN}" == *"cp38"* ]] || \
+       [[ "${PYBIN}" == *"cp39"* ]]; then
+        cd /io
+        "${PYBIN}/pip" install --upgrade pip wheel
+        "${PYBIN}/pip" install .[test]
+        "${PYBIN}/python" setup.py bdist_wheel
+        "${PYBIN}/python" setup.py test
+
+        #"${PYBIN}/pip" install numpy scipy
+        #"${PYBIN}/pip" install -e /io/
+        #(cd /io && "${PYBIN}/python" setup.py test)
+        #"${PYBIN}/pip" wheel /io/ -w dist/
+        #rm -rf /io/build /io/*.egg-info
     fi
 done
+
+pwd
+ls -la dist
+ls -la /io/dist
 
 # Bundle external shared libraries into the wheels
 for whl in dist/fastcluster*.whl; do
