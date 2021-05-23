292bf2ef9b421bef99372cbdf3723faa9b27fa48
Daniel Müllner <daniel@danifold.net>
2021-05-23 19:41:31 +0200

    Update.

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -2,12 +2,9 @@
 
 set -e -x
 
-arch=$(uname -m)
-
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    if [[ "${PYBIN}" == *"cp36"* && $arch != "aarch64" ]] || \
-       [[ "${PYBIN}" == *"cp37"* ]] || \
+    if [[ "${PYBIN}" == *"cp37"* ]] || \
        [[ "${PYBIN}" == *"cp38"* ]] || \
        [[ "${PYBIN}" == *"cp39"* ]]; then
         cd /io
@@ -15,21 +12,11 @@ for PYBIN in /opt/python/*/bin; do
         "${PYBIN}/pip" install .[test]
         "${PYBIN}/python" setup.py bdist_wheel
         "${PYBIN}/python" setup.py test
-
-        #"${PYBIN}/pip" install numpy scipy
-        #"${PYBIN}/pip" install -e /io/
-        #(cd /io && "${PYBIN}/python" setup.py test)
-        #"${PYBIN}/pip" wheel /io/ -w dist/
-        #rm -rf /io/build /io/*.egg-info
+        false
     fi
 done
 
-pwd
-ls -la dist
-ls -la /io
-ls -la /io/dist
-
 # Bundle external shared libraries into the wheels
 for whl in dist/fastcluster*.whl; do
-    auditwheel repair "$whl" -w /io/dist/
+    auditwheel repair "$whl"
 done
