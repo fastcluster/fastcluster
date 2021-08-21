edc925a126763583233fb0f25b8d3a7cff8142b6
Daniel Müllner <daniel@danifold.net>
2021-08-21 22:57:25 +0200

    Update.

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -7,20 +7,20 @@ cd /io
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     if [[ "${PYBIN}" != *"cp37"* ]] && \
-       [[ "${PYBIN}" == *"cp38"* ]] && \
-       [[ "${PYBIN}" == *"cp39"* ]]; then
+       [[ "${PYBIN}" != *"cp38"* ]] && \
+       [[ "${PYBIN}" != *"cp39"* ]]; then
         continue
     fi
 
     "${PYBIN}/pip" install --upgrade pip wheel
 
-    if [[ "${PYBIN}" != *"cp37"* ]]; then
+    if [[ "${PYBIN}" == *"cp37"* ]]; then
         "${PYBIN}/pip" install numpy==1.15.0
     fi
-    if [[ "${PYBIN}" != *"cp38"* ]]; then
+    if [[ "${PYBIN}" == *"cp38"* ]]; then
         "${PYBIN}/pip" install numpy==1.17.3
     fi
-    if [[ "${PYBIN}" != *"cp39"* ]]; then
+    if [[ "${PYBIN}" == *"cp39"* ]]; then
         "${PYBIN}/pip" install numpy==1.19.3
     fi
 
