23247d42bbe9a9cae1b4ee4ddd468cb27a4b21d5
Daniel Müllner <daniel@danifold.net>
2021-08-21 22:30:02 +0200

    Update.

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -6,14 +6,27 @@ cd /io
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    if [[ "${PYBIN}" == *"cp37"* ]] || \
-       [[ "${PYBIN}" == *"cp38"* ]] || \
+    if [[ "${PYBIN}" != *"cp37"* ]] && \
+       [[ "${PYBIN}" == *"cp38"* ]] && \
        [[ "${PYBIN}" == *"cp39"* ]]; then
-        "${PYBIN}/pip" install --upgrade pip wheel
-        "${PYBIN}/pip" install .[test]
-        "${PYBIN}/python" setup.py bdist_wheel
-        "${PYBIN}/python" setup.py test
+        continue
     fi
+
+    "${PYBIN}/pip" install --upgrade pip wheel
+
+    if [[ "${PYBIN}" != *"cp37"* ]]; then
+        "${PYBIN}/pip" install numpy==1.15.0
+    fi
+    if [[ "${PYBIN}" != *"cp38"* ]]; then
+        "${PYBIN}/pip" install numpy==1.17.3
+    fi
+    if [[ "${PYBIN}" != *"cp39"* ]]; then
+        "${PYBIN}/pip" install numpy==1.19.3
+    fi
+
+    "${PYBIN}/pip" install .[test]
+    "${PYBIN}/python" setup.py bdist_wheel
+    "${PYBIN}/python" setup.py test
 done
 
 # Bundle external shared libraries into the wheels
