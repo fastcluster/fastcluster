2fbf4e1708589ab5d294c422ca98c603deb91fcd
Daniel Müllner <daniel@danifold.net>
2022-02-26 19:20:08 +0100

    Simplify Manylinux script.

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -2,26 +2,23 @@
 
 set -e -x
 
-cd /io
-
 ls -l /opt/python
+
+cd /io
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    if [[ "${PYBIN}" != *"cp38"* ]] && \
-       [[ "${PYBIN}" != *"cp39"* ]] && \
-       [[ "${PYBIN}" != *"cp310"* ]]; then
+    if [[ "${PYBIN}" != *"cp3"* ]] || \
+       [[ "${PYBIN}" == *"cp36"* ]] || \
+       [[ "${PYBIN}" == *"cp37"* ]]; then
         continue
     fi
 
-    "${PYBIN}/python" -m venv fastcluster-env
-    ls -l fastcluster-env/bin
-    source fastcluster-env/bin/activate
-    pip install --upgrade pip wheel
-    pip install oldest_supported_numpy
-    pip install .[test]
-    python setup.py bdist_wheel
-    python setup.py test
+    "${PYBIN}/pip" install --upgrade pip wheel
+    "${PYBIN}/pip" install oldest_supported_numpy
+    "${PYBIN}/pip" install .[test]
+    "${PYBIN}/python" setup.py bdist_wheel
+    "${PYBIN}/python" setup.py test
 done
 
 # Bundle external shared libraries into the wheels
