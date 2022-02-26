fae8f1b7ffcb7ca079fdf105fd1b0e6791034f14
Daniel Müllner <daniel@danifold.net>
2022-02-26 13:52:12 +0100

    Try Python virtual environment in Travis builds.

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -9,15 +9,18 @@ ls -l /opt/python
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
     if [[ "${PYBIN}" != *"cp38"* ]] && \
-       [[ "${PYBIN}" != *"cp39"* ]]; then
+       [[ "${PYBIN}" != *"cp39"* ]] && \
+       [[ "${PYBIN}" != *"cp310"* ]]; then
         continue
     fi
 
-    "${PYBIN}/pip" install --upgrade pip wheel
-    "${PYBIN}/pip" install oldest_supported_numpy
-    "${PYBIN}/pip" install .[test]
-    "${PYBIN}/python" setup.py bdist_wheel
-    "${PYBIN}/python" setup.py test
+    "${PYBIN}/python" -m venv fastcluster-env
+    fastcluster-env/bin/activate
+    pip install --upgrade pip wheel
+    pip install oldest_supported_numpy
+    pip install .[test]
+    python setup.py bdist_wheel
+    python setup.py test
 done
 
 # Bundle external shared libraries into the wheels
