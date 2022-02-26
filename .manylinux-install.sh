c651877e39cc489171b4de7ad1312ed6a6057fd2
Daniel Müllner <daniel@danifold.net>
2022-02-26 13:32:35 +0100

    Fix Travis build.

--- .manylinux-install.sh
+++ .manylinux-install.sh
@@ -4,26 +4,17 @@ set -e -x
 
 cd /io
 
+ls -l /opt/python
+
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    if [[ "${PYBIN}" != *"cp37"* ]] && \
-       [[ "${PYBIN}" != *"cp38"* ]] && \
+    if [[ "${PYBIN}" != *"cp38"* ]] && \
        [[ "${PYBIN}" != *"cp39"* ]]; then
         continue
     fi
 
     "${PYBIN}/pip" install --upgrade pip wheel
-
-    if [[ "${PYBIN}" == *"cp37"* ]]; then
-        "${PYBIN}/pip" install numpy==1.15.0
-    fi
-    if [[ "${PYBIN}" == *"cp38"* ]]; then
-        "${PYBIN}/pip" install numpy==1.17.3
-    fi
-    if [[ "${PYBIN}" == *"cp39"* ]]; then
-        "${PYBIN}/pip" install numpy==1.19.3
-    fi
-
+    "${PYBIN}/pip" install oldest_supported_numpy
     "${PYBIN}/pip" install .[test]
     "${PYBIN}/python" setup.py bdist_wheel
     "${PYBIN}/python" setup.py test
